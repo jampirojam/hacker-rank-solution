4bf472786036bfab8bedd685eb99a89206ebd473
M. Machrush Aliy Sirojjam Mushlich <machrojamz@gmail.com>
2022-02-10 21:47:15 +0700

    Create Weather Observation Station 10.sql

---
+++ SQL/Basic Select/Weather Observation Station 10.sql
@@ -0,0 +1,5 @@
+# Soal 15 - JAMPIROJAM
+select distinct CITY from STATION where right(CITY,1) not in('a','e','i','o','u');
+
+# Juga bisa gunakan LIKE
+select distinct(CITY) from STATION where CITY not like '%a' and CITY not like '%e' and CITY not like '%i' and CITY not like '%o' and CITY not like '%u';
