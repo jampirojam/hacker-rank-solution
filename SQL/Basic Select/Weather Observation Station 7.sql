b477bd69a7a976b282aa81ada9832120a1d7d732
M. Machrush Aliy Sirojjam Mushlich <machrojamz@gmail.com>
2022-02-10 21:40:48 +0700

    Update Weather Observation Station 7.sql

--- SQL/Basic Select/Weather Observation Station 7.sql
+++ SQL/Basic Select/Weather Observation Station 7.sql
@@ -2,4 +2,4 @@
 select distinct CITY from STATION where right(CITY,1) in('a','e','i','o','u');
 
 # Juga bisa gunakan LIKE
-select distinct(CITY) from STATION where CITY like '%a' or city like '%e' or city like '%i' or city like '%o' or city like '%u';
+select distinct(CITY) from STATION where CITY like '%a' or CITY like '%e' or CITY like '%i' or CITY like '%o' or CITY like '%u';
