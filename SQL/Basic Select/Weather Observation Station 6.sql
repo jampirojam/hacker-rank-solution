f131b42119a6e79417e4b30612232c67a87f83e3
M. Machrush Aliy Sirojjam Mushlich <machrojamz@gmail.com>
2022-02-10 21:23:56 +0700

    Create Weather Observation Station 6.sql

---
+++ SQL/Basic Select/Weather Observation Station 6.sql
@@ -0,0 +1,5 @@
+# Soal 11 - JAMPIROJAM
+select distinct CITY from STATION where left(CITY,1) in('a','e','i','o','u');
+
+# Juga bisa gunakan LIKE
+select distinct(CITY) from STATION where CITY like "a%" or city like "e%" or city like "i%" or city like "o%" or city like "u%";
