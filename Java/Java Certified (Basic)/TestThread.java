b148302dfbe087097f28279877fdc8cac7f5097f
M. Machrush Aliy Sirojjam Mushlich <jampirojam@gmail.com>
2022-02-16 04:55:07 +0700

    Create TestThread.java

---
+++ Java/Java Certified (Basic)/TestThread.java
@@ -0,0 +1,37 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+class SampleDemo implements Runnable {
+    private Thread t;
+    private String threadName;
+
+    SampleDemo(String threadName) {
+        this.threadName = threadName;
+    }
+
+    @Override
+    public void run() {
+        while (true) {
+            System.out.println(threadName);
+        }
+    }
+
+    public void start() {
+        if (t == null) {
+            t = new Thread(this, threadName);
+            t.start();
+        }
+    }
+}
+
+public class TestThread {
+    public static void main(String[] args) {
+        SampleDemo A = new SampleDemo("A");
+        SampleDemo B = new SampleDemo("B");
+        B.start();
+        A.start();
+    }
+}
