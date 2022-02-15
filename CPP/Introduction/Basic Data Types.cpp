4ef5f4ca932e74868bf3aae3449e8aaf9212012e
M. Machrush Aliy Sirojjam Mushlich <jampirojam@gmail.com>
2022-02-16 06:34:13 +0700

    Create Basic Data Types.cpp

---
+++ CPP/Introduction/Basic Data Types.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cstdio>
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    // Complete the code.
+    int a; long b; char c; float d; double e;
+    cin>>a>>b>>c>>d>>e;
+    cout<<a<<"\n"<<b<<"\n"<<c<<"\n";
+    cout<<fixed<<setprecision(3)<<d<<"\n"; //membuat 3 angka di belakang koma
+    cout<<fixed<<setprecision(9)<<e<<"\n"; //membuat 9 angka di belakang koma
+    return 0;
+}
